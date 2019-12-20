92c4febf2a13b4c7d43e5fee06569403df004993
João Trindade <joaotrindade.soft@gmail.com>
2019-12-20 11:47:07 +0100

    resolve issues

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-import styled, * as othersFeatures from "styled-components";
-import View from "./lib/index.js";
-const styledComponents = {
-  styled,
-  others: othersFeatures
-};
-export { styledComponents };
-export default View;
